e80185207b8ce405e17eecdc402a4ba670f1757c
Schpser <schps.games@gmail.com>
2025-10-29 00:50:20 +0100

    Add Bcrypt

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,18 +1,24 @@
 from flask import Flask
+from flask_bcrypt import Bcrypt
 from flask_restx import Api
 from app.api.v1.users import user_namespace as users_ns
 from app.api.v1.amenities import amenity_namespace
 from app.api.v1.places import place_namespace
 from app.api.v1.reviews import review_namespace
 
+bcrypt = Bcrypt()
+
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
+    bcrypt.init_app(app)
+    
     api.add_namespace(users_ns, path='/api/v1/users')
     api.add_namespace(amenity_namespace, path='/api/v1/amenities')
     api.add_namespace(place_namespace, path='/api/v1/places')
     api.add_namespace(review_namespace, path='/api/v1/reviews')
     
+
     return app
