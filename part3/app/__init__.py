2614c016531b3a859420904dd5d66e069ffd6c99
Schpser <schps.games@gmail.com>
2025-10-29 14:49:43 +0100

    Handle JWT

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -5,20 +5,24 @@ from app.api.v1.users import user_namespace as users_ns
 from app.api.v1.amenities import amenity_namespace
 from app.api.v1.places import place_namespace
 from app.api.v1.reviews import review_namespace
+from app.api.v1.auth import api as auth_ns
+from flask_jwt_extended import JWTManager
 
-bcrypt = Bcrypt()
+jwt = JWTManager()
+bcrypt = Bcrypt(None)
 
 def create_app(config_class="config.DevelopmentConfig"):
-    app = Flask(__name__)
+    app = Flask('app')
     app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API', doc='/api/v1/')
 
     bcrypt.init_app(app)
-    
+    jwt.init_app(app)
+
     api.add_namespace(users_ns, path='/api/v1/users')
+    api.add_namespace(auth_ns, path='/api/v1/auth')
     api.add_namespace(amenity_namespace, path='/api/v1/amenities')
     api.add_namespace(place_namespace, path='/api/v1/places')
     api.add_namespace(review_namespace, path='/api/v1/reviews')
     
-
     return app
